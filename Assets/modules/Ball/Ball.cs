b24947bb3cb949cc3b1e160ff731c6484a6296d1
Luca Barone <baro.luc@gmail.com>
2020-12-31 10:37:54 +0100

    Add Ball, Paddle & Collisions in the scene

--- Assets/modules/Ball/Ball.cs
+++ Assets/modules/Ball/Ball.cs
@@ -1,5 +1,15 @@
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
-    
+public class Ball : MonoBehaviour
+{
+    void Start()
+    {
+        var cam = Camera.main;
+        var bottomLeftCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(0, 0));
+        var topRightCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        this.transform.position = Vector2.Lerp(bottomLeftCorner, topRightCorner, 0.5f);
+
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
 }
--- Assets/modules/Paddle/Paddle.cs
+++ Assets/modules/Paddle/Paddle.cs
@@ -1,5 +1,25 @@
 using UnityEngine;
 
-public class Paddle : MonoBehaviour {
-    
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    bool isPlayer = false;
+
+    int offset = 1;
+
+    void Start()
+    {
+        var cam = Camera.main;
+        var bottomLeftCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(0, 0));
+        var topRightCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        var bottomRightCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(Screen.width, 0));
+        var width = Vector2.Distance(bottomLeftCorner, bottomRightCorner);
+        var center = Vector2.Lerp(bottomLeftCorner, topRightCorner, 0.5f);
+
+        this.transform.localScale = new Vector2(width / 4, 0.5f);
+
+        this.transform.position = new Vector2(center.x, isPlayer ? bottomLeftCorner.y + offset : topRightCorner.y - offset);
+
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
 }
