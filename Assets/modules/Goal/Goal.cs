dc31375e2d9e698d8fe8f391268a6dfc686df27d
Luca Barone <baro.luc@gmail.com>
2020-12-31 10:20:58 +0100

    Scale & Position Walls & Goals

--- Assets/modules/Goal/Goal.cs
+++ Assets/modules/Goal/Goal.cs
@@ -1,5 +1,26 @@
 using UnityEngine;
 
-public class Goal : MonoBehaviour {
-    
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    bool isNorth = true;
+
+    Vector2 offset = new Vector2(0, 0.5f);
+
+    void Start()
+    {
+        var cam = Camera.main;
+        var bottomLeftCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(0, 0));
+        var topRightCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        var bottomRightCorner = new Vector2(topRightCorner.x, bottomLeftCorner.y);
+        var width = Vector2.Distance(bottomLeftCorner, bottomRightCorner);
+
+        this.transform.localScale = new Vector2(width, 1);
+
+        this.transform.position = isNorth
+            ? new Vector2(0, topRightCorner.y) + this.offset
+            : new Vector2(0, bottomLeftCorner.y) - this.offset;
+
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
 }
--- Assets/modules/Wall/Wall.cs
+++ Assets/modules/Wall/Wall.cs
@@ -1,5 +1,26 @@
 using UnityEngine;
 
-public class Wall : MonoBehaviour {
-    
+public class Wall : MonoBehaviour
+{
+    [SerializeField]
+    bool isWest = true;
+
+    Vector2 offset = new Vector2(0.5f, 0);
+
+    void Start()
+    {
+        var cam = Camera.main;
+        var bottomLeftCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(0, 0));
+        var topRightCorner = (Vector2)cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        var height = Vector2.Distance(bottomLeftCorner, new Vector2(bottomLeftCorner.x, topRightCorner.y));
+
+
+        this.transform.localScale = new Vector2(1, height);
+
+        this.transform.position = isWest
+            ? new Vector2(bottomLeftCorner.x, 0) - this.offset
+            : new Vector2(topRightCorner.x, 0) + this.offset;
+
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
 }
