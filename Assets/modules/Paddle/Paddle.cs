1a1a7f6b579d7afe3e75ac845258c3da255544c2
Luca Barone <baro.luc@gmail.com>
2021-01-05 09:26:59 +0100

    Remove movement logic from paddle script

--- Assets/modules/Paddle/Paddle.cs
+++ Assets/modules/Paddle/Paddle.cs
@@ -7,41 +7,10 @@ public class Paddle : MonoBehaviour
     bool isPlayer = false;
 
     int offset = 1;
-
-    Vector2? lastMousePosition;
-
     Camera cam;
-
-    Rigidbody2D rb;
-
-    void HandlePlayerMovement()
-    {
-        if (!this.isPlayer)
-        {
-            return;
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            var mousePosition = this.cam.ScreenToWorldPoint(Input.mousePosition);
-
-            if (this.lastMousePosition != null)
-            {
-                var delta = new Vector2(mousePosition.x - (float)this.lastMousePosition?.x, 0f);
-                this.rb.velocity += delta;
-            }
-
-            this.lastMousePosition = mousePosition;
-        }
-        else
-        {
-            this.lastMousePosition = null;
-        }
-    }
 
     void Start()
     {
-        this.rb = GetComponent<Rigidbody2D>();
         this.cam = Camera.main;
         var bottomLeftCorner = (Vector2)this.cam.ScreenToWorldPoint(new Vector2(0, 0));
         var topRightCorner = (Vector2)this.cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
@@ -53,10 +22,5 @@ public class Paddle : MonoBehaviour
         this.transform.position = new Vector2(center.x, this.isPlayer ? bottomLeftCorner.y + this.offset : topRightCorner.y - this.offset);
 
         GetComponent<SpriteRenderer>().enabled = true;
-    }
-
-    void Update()
-    {
-        this.HandlePlayerMovement();
     }
 }
